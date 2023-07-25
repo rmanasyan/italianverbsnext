9bc5c23beca130af098b2531ba15fdde1bf8983c
Roman Manasyan <rmanasyan@gmail.com>
2023-07-25 09:35:49 +0300

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { ReactNode } from 'react'
 import { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 import { twMerge } from 'tailwind-merge'
 import { fontHeading, fontSans } from '@/utils/fonts'
 import { Footer } from '@/components/footer'
@@ -34,6 +35,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <main className={'my-8 grow'}>{children}</main>
           <Footer />
         </div>
+        <Analytics />
       </body>
     </html>
   )
