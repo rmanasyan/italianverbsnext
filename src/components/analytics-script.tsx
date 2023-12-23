8d62303aab9b7fe90b367fe848706d8efbd73456
Roman Manasyan <rmanasyan@gmail.com>
2023-12-24 00:03:47 +0200

    chore: umami cloud

--- src/components/analytics-script.tsx
+++ src/components/analytics-script.tsx
@@ -9,7 +9,8 @@ export function AnalyticsScript() {
 
   return (
     <>
-      <Script src="https://umami.mnsn.pro/script.js" data-website-id="e8159e7b-eb86-431c-bac5-dc9f1deefd59" />
+      {/*<Script src="https://umami.mnsn.pro/script.js" data-website-id="e8159e7b-eb86-431c-bac5-dc9f1deefd59" />*/}
+      <Script src="https://eu.umami.is/script.js" data-website-id="d25e451b-c4b0-4c02-a493-5f5a13c1c1c6" />
     </>
   )
 }
