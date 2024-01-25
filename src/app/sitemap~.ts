6f9f998c3f294f9bcb6fe1c5328694b9487e49dc
Roman Manasyan <rmanasyan@gmail.com>
2024-01-25 14:28:47 +0200

    chore: npm update

--- src/app/sitemap~.ts
+++ src/app/sitemap~.ts
@@ -1,11 +1,12 @@
 import { MetadataRoute } from 'next'
 import { getVerbs } from '@/db/data'
+import { Verb } from '@/types/verbs'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const verbs = await getVerbs()
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || ''
 
-  const sitemapData = verbs.map((verb) => {
+  const sitemapData = verbs.map((verb: Verb) => {
     return {
       url: `${baseUrl}/${verb.verb}`,
       lastModified: new Date()
